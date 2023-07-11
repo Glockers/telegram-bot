e9660d6c886dae2d649babf27a1594d3cb076b8d
Glockers <megamaxchanel@gmail.com>
2023-07-11 17:36:20 +0300

    feature: add config service

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,7 +1,20 @@
+import { DotenvParseOutput, config } from 'dotenv';
 import { IConfigService } from './config.interface';
 
 export class ConfigService implements IConfigService {
+  private config: DotenvParseOutput;
+
+  constructor() {
+    const { error, parsed } = config();
+    if (error) throw new Error('Не найден file dotend');
+    if (!parsed) throw new Error('Пусто .env file');
+
+    this.config = parsed;
+  }
+
   get(key: string): string {
-    throw new Error('Method not implemented.');
+    const findedKey = this.config[key];
+    if (!findedKey) throw new Error(`Ключ: ${key} не найден`);
+    return findedKey;
   }
 }
