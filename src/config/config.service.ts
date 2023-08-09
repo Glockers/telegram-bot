2a689ed055e06e897e8ef6eda8f824a496e0ec36
Glockers <megamaxchanel@gmail.com>
2023-08-09 13:39:08 +0300

    fix: change logic convif service

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,5 +1,5 @@
-import { DotenvParseOutput, config } from 'dotenv';
 import { injectable } from 'inversify';
+import 'dotenv/config';
 
 export interface IConfigService {
   get(key: string): string;
@@ -7,19 +7,11 @@ export interface IConfigService {
 
 @injectable()
 export class ConfigService implements IConfigService {
-  private config: DotenvParseOutput;
-
-  constructor() {
-    const { error, parsed } = config();
-    if (error) throw new Error('Не найден file dotend');
-    if (!parsed) throw new Error('Пусто .env file');
-
-    this.config = parsed;
-  }
-
   get(key: string): string {
-    const findedKey = this.config[key];
-    if (!findedKey) throw new Error(`Ключ: ${key} не найден`);
-    return findedKey;
+    const value = process.env[key];
+    if (value === undefined) {
+      throw new Error(`Переменная окружения: ${key} не найдена`);
+    }
+    return value;
   }
 }
